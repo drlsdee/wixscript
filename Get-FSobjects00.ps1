d3b76308fd10a41f8c989b9b5b612c62d10a151d
drlsdee <tracert0@gmail.com>
2019-04-10 13:57:35 +0500

    'Depth' fixed, 'Type' - changed to [string]

--- Get-FSobjects00.ps1
+++ Get-FSobjects00.ps1
@@ -1,9 +1,4 @@
-﻿enum FSItemType {
-    file
-    folder
-}
-
-enum YesNoType {
+﻿enum YesNoType {
     yes
     no
 }
@@ -13,17 +8,35 @@ class FSobject {
     [string]$Name
     [string]$Path
     [string]$Source
-    [FSItemType]$Type
+    [string]$Type # TODO ValidateSet
     [array]$SubItems
     [int]$Depth
 
-    [void]FillSubItems(){
-        $i = 0
-        While ($i -lt $this.SubItems.Count) {
-            $j = Get-Item ($this.SubItems[$i] | Resolve-Path -Relative)
-            $j = $this::new($j)
-            $this.SubItems[$i] = $j
-            $i++
+    [void] GetChildItems () {
+        $count = 0
+        while ($count -lt $this.SubItems.Count) {
+            $item = Get-Item ($this.SubItems[$count] | Resolve-Path -Relative)
+            $item = $this::new($item)
+            $this.SubItems[$count] = $item
+            $count++
+        }
+    }
+
+    [void] GetItemsRecursively () {
+        Write-Host "Current OBJECT is" $this.Name
+        $this.GetChildItems()
+        $items = $this.SubItems
+        Write-Host $items.Name
+        if ($items.Count -gt $null) {
+            $count = 0
+            while ($count -lt $items.Count) {
+                $items[$count].GetChildItems()
+                Write-Host $items[$count].Name
+                $count++
+            }
+            foreach ($item in $items) {
+                $item.GetItemsRecursively()
+            }
         }
     }
 
@@ -34,19 +47,20 @@ class FSobject {
         $this.Id = $obj.BaseName
         $this.Name = $obj.Name
         $this.Path = $obj | Resolve-Path -Relative
+        $this.Depth = 1
         If (Test-Path -Path $obj -PathType Container) {
             $this.Type = "folder"
             $this.Source = $null
-            $this.SubItems = Get-ChildItem -Path $this.Path
+            $this.SubItems = Get-ChildItem -Path $this.Path #| Where-Object {$_.Attributes -notmatch $ignoreList}
         } Else {
             $this.Type = "file"
             $this.Source = $this.Path -creplace '^[^\\]*\\', ''
             $this.SubItems = $null
         }
-        $this.Depth = 1
-        $this.FillSubItems()
-        if (($this.SubItems | Where-Object Type -EQ "folder").Count) {
-            $this.Depth += $this.SubItems[0].Depth
+        $this.GetChildItems()
+        if ($this.SubItems) {
+            $subItemDepth = $this.SubItems | Measure-Object -Property Depth -Maximum
+            $this.Depth += $subItemDepth.Maximum
         }
     }
 }
@@ -73,12 +87,18 @@ class Package {
     [int]$SummaryCodepage
 }
 
-$MainExecName = "maincontroller.exe"
+#$MainExecName = "maincontroller.exe"
+
+#$ignoreList = @("ReparsePoint")
 
 $Location = "C:\Users\Administrator\Desktop\AZK_History\1"
-$MainExecutable = Get-ChildItem -Path $Location -Recurse -Filter $MainExecName
+#$MainExecutable = Get-ChildItem -Path $Location -Recurse -Filter $MainExecName
 Set-Location $Location
 $RootDir = Get-Item $Location
 $RootDir = [FSobject]::new($RootDir)
+$testXMLDoc = New-Object -TypeName System.Xml.XmlDocument
 
-$RootDir.Depth
+$tempOut = $RootDir | ConvertTo-Xml -Depth $RootDir.Depth -NoTypeInformation -As String
+$tempOut
+$testXMLDoc.LoadXml($tempOut)
+$testXMLDoc.Save('C:\testXMLdoc.xml')
